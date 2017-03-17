6ca0c38d4ea09a9908ac477385f12a86944a532f
Rolind Roy <rolind.roy@gmail.com>
2017-03-17 09:59:08 +0530

    added files

---
+++ python-git-sync.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import git
+
+app = Flask(__name__)
+api = Api(app)
+
+root_dir = "/webadmin/"
+
+class PullRequest(Resource):
+    def get(self, resource_id):
+        dest_name = root_dir + resource_id
+        repo = git.Repo(dest_name)
+        repo_data = repo.remotes.origin
+        sync = repo_data.pull();
+        return {'Message': 'Done'}
+api.add_resource(PullRequest, '/<string:resource_id>')
+
+if __name__ == '__main__':
+#    app.run(host='0.0.0.0')
+    app.run(debug=True)
+

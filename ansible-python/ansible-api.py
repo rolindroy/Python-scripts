042f364aebabcb1fecbff67343e23e1755228bab
Rolind Roy <rolind.roy@gmail.com>
2017-03-23 17:54:46 +0530

    added files

--- ansible-python/ansible-api.py
+++ ansible-python/ansible-api.py
@@ -27,7 +27,7 @@ class PullRequest(Resource):
             sys.exit()
 
         Options = namedtuple('Options', ['listtags', 'listtasks', 'listhosts', 'syntax', 'connection','module_path', 'forks', 'remote_user', 'private_key_file', 'ssh_common_args', 'ssh_extra_args', 'sftp_extra_args', 'scp_extra_args', 'become', 'become_method', 'become_user', 'verbosity', 'check'])
-        options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100, remote_user='ubuntu', private_key_file=None, ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=True, become_method=None, become_user='root', verbosity=None, check=False)
+        options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100, remote_user='ubuntu', private_key_file=None, ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=True, become_method=None, become_user='root', verbosity=3, check=False)
 
         variable_manager.extra_vars = {'target_host': request.remote_addr} # This can accomodate various other command line arguments.`
 

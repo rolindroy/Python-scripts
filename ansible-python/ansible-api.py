749ac899c55f0819c657c6c9e4a71fea5bd68485
Rolind Roy <rolind.roy@gmail.com>
2017-05-18 17:54:11 +0530

    Handling empty username

--- ansible-python/ansible-api.py
+++ ansible-python/ansible-api.py
@@ -21,13 +21,17 @@ class PullRequest(Resource):
 
         inventory = Inventory(loader=loader, variable_manager=variable_manager, host_list='./hosts')
         playbook_path = './playbook.yml'
+	remoteuser = 'ubuntu_user' # Modify for default user
+	
+	if user_name:
+	    remoteuser = user_name 
 
         if not os.path.exists(playbook_path):
             print '[INFO] The playbook does not exist'
             sys.exit()
 
         Options = namedtuple('Options', ['listtags', 'listtasks', 'listhosts', 'syntax', 'connection','module_path', 'forks', 'remote_user', 'private_key_file', 'ssh_common_args', 'ssh_extra_args', 'sftp_extra_args', 'scp_extra_args', 'become', 'become_method', 'become_user', 'verbosity', 'check'])
-        options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100, remote_user=user_name, private_key_file=None, ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=True, become_method=None, become_user='root', verbosity=4, check=False)
+        options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100, remote_user=remoteuser, private_key_file=None, ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=True, become_method=None, become_user='root', verbosity=4, check=False)
 
         variable_manager.extra_vars = {'target_host': request.remote_addr} # This can accomodate various other command line arguments.`
 
